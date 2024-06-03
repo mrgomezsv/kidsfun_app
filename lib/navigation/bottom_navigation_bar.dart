028ff63c584e953090501790bd40881df9823de7
Mario Roberto <mrgomez.dev@gmail.com>
2024-06-03 10:04:06 -0600

    Add indicador in bottom navigation bar

--- lib/navigation/bottom_navigation_bar.dart
+++ lib/navigation/bottom_navigation_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyBottomNavigationBar extends StatelessWidget {
+class MyBottomNavigationBar extends StatefulWidget {
   final int currentIndex;
   final Function(int) onTap;
 
@@ -10,11 +10,52 @@ class MyBottomNavigationBar extends StatelessWidget {
   });
 
   @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: widget.currentIndex.toDouble(), end: widget.currentIndex.toDouble()).animate(_controller);
+  }
+
+  @override
+  void didUpdateWidget(covariant MyBottomNavigationBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentIndex != widget.currentIndex) {
+      _controller.reset();
+      _animation = Tween<double>(begin: oldWidget.currentIndex.toDouble(), end: widget.currentIndex.toDouble()).animate(_controller)
+        ..addListener(() {
+          setState(() {});
+        });
+      _controller.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
+    widget.onTap(index);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
         margin: EdgeInsets.all(5.0), // Margen para separar la barra de los bordes de la pantalla
-        height: 60, // Altura ajustada para evitar el desbordamiento
+        height: 70, // Altura ajustada para evitar el desbordamiento
         decoration: BoxDecoration(
           color: Colors.blue, // Color de fondo azul
           borderRadius: BorderRadius.all(
@@ -28,32 +69,66 @@ class MyBottomNavigationBar extends StatelessWidget {
             ),
           ],
         ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.all(
-            Radius.circular(30.0), // Asegura que los bordes redondeados se apliquen al contenido
-          ),
-          child: BottomNavigationBar(
-            backgroundColor: Colors.transparent, // Fondo transparente para ver el fondo azul del contenedor
-            currentIndex: currentIndex,
-            onTap: onTap,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart, size: 26, color: Colors.white),
-                label: '',
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(3, (index) {
+                  return Expanded(
+                    child: GestureDetector(
+                      onTap: () => _onItemTapped(index),
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: AnimatedBuilder(
+                          animation: _animation,
+                          builder: (context, child) {
+                            return Container(
+                              margin: EdgeInsets.symmetric(horizontal: 5.0),
+                              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30.0),
+                                border: Border.all(
+                                  color: _animation.value == index ? Colors.white : Colors.transparent,
+                                  width: 2,
+                                ),
+                              ),
+                              child: child,
+                            );
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                index == 0
+                                    ? Icons.shopping_cart
+                                    : index == 1
+                                    ? Icons.local_offer
+                                    : Icons.person,
+                                size: 26,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                index == 0
+                                    ? 'Cart'
+                                    : index == 1
+                                    ? 'Offers'
+                                    : 'Profile',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.local_offer, size: 26, color: Colors.white),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, size: 26, color: Colors.white),
-                label: '',
-              ),
-            ],
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            type: BottomNavigationBarType.fixed, // Esto asegura que los iconos estén centrados
-          ),
+            ),
+          ],
         ),
       ),
     );
