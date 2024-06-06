6f73835900b5df76f59fb842bbedd80535b06d3a
Mario Roberto <mrgomez.dev@gmail.com>
2024-06-06 00:45:27 -0600

    Add container inside user name and image of place holder user

--- lib/screen_principal/product_screen.dart
+++ lib/screen_principal/product_screen.dart
@@ -20,34 +20,63 @@ class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Products'),
-      // ),
-      body: FutureBuilder<List<Product>>(
-        future: _futureProducts,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            return GridView.count(
-              crossAxisCount: 2, // Aquí estableces el número de elementos por fila
-              children: snapshot.data!.map((product) {
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ProductCard(
-                    product: product,
-                    onFavoriteToggle: () {
-                      // Lógica para alternar favoritos
-                    },
-                    isFavorite: false, // Cambia esto si tienes un sistema de favoritos
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.inversePrimary, // Aplicando el mismo color que el AppBar
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              ),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30, // Tamaño del círculo de la foto del usuario
+                  backgroundImage: AssetImage('assets/images/user_profile.jpeg'), // Placeholder de la imagen del usuario
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Nombre del Usuario',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
                   ),
-                );
-              }).toList(),
-            );
-          }
-        },
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<Product>>(
+              future: _futureProducts,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else {
+                  return GridView.count(
+                    crossAxisCount: 2, // Aquí estableces el número de elementos por fila
+                    children: snapshot.data!.map((product) {
+                      return Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ProductCard(
+                          product: product,
+                          onFavoriteToggle: () {
+                            // Lógica para alternar favoritos
+                          },
+                          isFavorite: false, // Cambia esto si tienes un sistema de favoritos
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screen_principal/ticket_screen.dart
+++ lib/screen_principal/ticket_screen.dart
@@ -3,8 +3,46 @@ import 'package:flutter/material.dart';
 class TicketScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Ticket Screen'),
+    return Scaffold(
+      body: Column(
+        children: [
+          // AppBar(
+          //   title: Text('Ticket Screen'), // Título del AppBar para la pantalla de tickets
+          //   backgroundColor: Theme.of(context).colorScheme.inversePrimary, // Color de fondo del AppBar
+          // ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.inversePrimary, // Aplicando el mismo color que el AppBar
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              ),
+            ),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30, // Tamaño del círculo de la foto del usuario
+                  backgroundImage: AssetImage('assets/images/user_profile.jpeg'), // Placeholder de la imagen del usuario
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'Nombre del Usuario',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Ticket Screen'), // Contenido de la pantalla de tickets
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
