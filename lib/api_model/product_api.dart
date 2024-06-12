2a1a790c981c6f8de951fec3dcacb6b58c75c49c
Mario Roberto <mrgomez.dev@gmail.com>
2024-06-12 17:51:00 -0600

    feat: Verificar autenticación de usuario antes de llamar a la API
    Se agregó verificación para asegurar que solo los usuarios autenticados en Firebase puedan acceder a la API de productos. Se utiliza FirebaseAuth para verificar la autenticación antes de realizar la solicitud HTTP.
    
    # Detalles adicionales:
    - Se importó FirebaseAuth para gestionar la autenticación.
    - Se lanzan excepciones adecuadas si el usuario no está autenticado o si falla la carga de productos desde la API.

--- lib/api_model/product_api.dart
+++ lib/api_model/product_api.dart
@@ -1,10 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:firebase_auth/firebase_auth.dart'; // Importa la librería de Firebase Auth
 import 'product_model.dart';
 
-
 class ProductApi {
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+
   static Future<List<Product>> fetchProducts() async {
+    // Verificar si el usuario está autenticado
+    User? user = _auth.currentUser;
+    if (user == null) {
+      throw Exception('User not authenticated');
+    }
+
+    // Usuario autenticado, proceder con la llamada a la API
     final response = await http.get(Uri.parse('https://kidsfunyfiestasinfantiles.webcraftedsolutions.net/api/products/'));
 
     if (response.statusCode == 200) {
