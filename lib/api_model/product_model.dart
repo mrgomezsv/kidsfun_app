ad84a602a73d11a9de432f519a04e4a79e27f0fe
Mario Roberto <mrgomez.dev@gmail.com>
2024-06-18 13:28:39 -0600

    Fix image URL concatenation issue in Product.fromJson method

--- lib/api_model/product_model.dart
+++ lib/api_model/product_model.dart
@@ -18,22 +18,35 @@ class Product {
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
+    String baseUrl = 'https://';
+
     List<String> images = [
-      json['img1'],
-      json['img2'],
-      json['img3'],
-      json['img4'],
-      json['img5'],
+      _completeUrl(baseUrl, json['img1']),
+      _completeUrl(baseUrl, json['img2']),
+      _completeUrl(baseUrl, json['img3']),
+      _completeUrl(baseUrl, json['img4']),
+      _completeUrl(baseUrl, json['img5']),
     ];
+
+    // Imprime las URLs para verificar
+    print('Main Image URL: ${_completeUrl(baseUrl, json['img'])}');
+    images.forEach((image) => print('Additional Image URL: $image'));
 
     return Product(
       id: json['id'],
-      img: json['img'],
+      img: _completeUrl(baseUrl, json['img']),
       additionalImages: images,
       title: json['title'],
       description: json['description'],
       price: json['price'],
       category: json['category'],
     );
+  }
+
+  static String _completeUrl(String baseUrl, String url) {
+    if (url.startsWith('http')) {
+      return url;
+    }
+    return baseUrl + url;
   }
 }
